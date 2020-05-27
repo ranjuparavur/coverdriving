7eb9a706d0358c7993f113ce65f5659cbc9cdd7e
Ranjith M <ranjuparavur@gmail.com>
2020-05-28 01:32:58 +0530

    updated the site.js file to change the title

--- config/site.js
+++ config/site.js
@@ -1,8 +1,8 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Gatsby Tutorial Starter', // Navigation and Site Title
-  titleAlt: 'Gatsby Tutorial Starter', // Title for JSONLD
-  description: 'A Gatsby V2 Starter Template Built with a Step By Step Guide',
+  title: 'Coverdriving', // Navigation and Site Title
+  titleAlt: 'Coverdriving', // Title for JSONLD
+  description: 'Everything Criket | Blog by Aniket Dass',
   url: 'https://gatsby-tutorial-starter.netlify.com', // Domain of your site. No trailing slash!
   siteUrl: 'https://gatsby-tutorial-starter.netlify.com', // url + pathPrefix
   siteLanguage: 'en', // Language Tag on <html> element
