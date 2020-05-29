02a2e8dd4184313e6fd9c8d0d77727e2df074c8d
Ranjith M <ranjuparavur@gmail.com>
2020-05-29 14:50:07 +0530

    Updated the site.js file to remove references to the template code

--- config/site.js
+++ config/site.js
@@ -3,16 +3,16 @@ module.exports = {
   title: 'Coverdriving', // Navigation and Site Title
   titleAlt: 'Coverdriving', // Title for JSONLD
   description: 'Everything Criket | Blog by Aniket Dass',
-  url: 'https://gatsby-tutorial-starter.netlify.com', // Domain of your site. No trailing slash!
-  siteUrl: 'https://gatsby-tutorial-starter.netlify.com', // url + pathPrefix
+  url: 'https://competent-rosalind-fddce3.netlify.app/', // Domain of your site. No trailing slash!
+  siteUrl: 'https://competent-rosalind-fddce3.netlify.app/', // url + pathPrefix
   siteLanguage: 'en', // Language Tag on <html> element
   logo: 'static/logo/logo.png', // Used for SEO
   banner: 'static/logo/cd_banner.jpg',
   // JSONLD / Manifest
   favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
-  shortName: 'GatsbyTut', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'Justin', // Author for schemaORGJSONLD
+  shortName: 'Coverdrive', // shortname for manifest. MUST be shorter than 12 characters
+  author: 'Ranjith', // Author for schemaORGJSONLD
   themeColor: '#3e7bf2',
   backgroundColor: '#d3e0ff',
-  twitter: '@justinformentin', // Twitter Username
+  twitter: '@coverdriving', // Twitter Username
 };
